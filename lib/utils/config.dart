0d2400629dd81c5010ff3d8028a093feeafd2852
Richard Albert Salendah <49094595+albertsalendah@users.noreply.github.com>
2024-05-04 22:45:39 +0700

    Update config.dart

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 class Configs {
-  static const String LINK = 'http://uksw-blast-api.marikhsalatiga.com/';
+  static const String LINK = '';
   static const String LOCAL_LINK = 'http://localhost:8080/';
   int logoutDuration = 60;
   Color green = const Color.fromRGBO(0, 167, 131, 1);
